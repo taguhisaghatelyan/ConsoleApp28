58f7e7ce43b196b3d300608f25370c942803a331
Sagha <Sagha@LAPTOP-AUS1OPRL>
2019-11-21 23:40:34 +0400

    ComplexNumbers

---
+++ ConsoleApp28/Complex.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Complex
+    {
+        public double Real { get; set; }
+        public double Im { get; set; }
+        public Complex()
+        {
+
+        }
+        public Complex (double real, double im)
+        {
+            Real = real;
+            Im = im;
+        }
+        public override string ToString()
+        {
+            return $"{Real}+{Im}i";
+        }
+
+        public static Complex Addition(Complex a,Complex b)
+        {
+            Complex sum = new Complex();
+            sum.Real= a.Real + b.Real;
+            sum.Im = a.Im + b.Im;
+            return sum;
+        }
+        public static Complex Subtraction(Complex a,Complex b)
+        {
+            Complex sub = new Complex();
+            sub.Real = a.Real - b.Real;
+            sub.Im = a.Im - b.Im;
+            return sub;
+        }
+        public static Complex Multiplication(Complex a,Complex b)
+        {
+            Complex mult = new Complex();
+            mult.Real = (a.Real * b.Real)-(a.Im*b.Im);
+            mult.Im = (a.Real * b.Im)+(a.Im*b.Real);
+            return mult;
+        }
+        public static Complex Division(Complex a, Complex b)
+        {
+            Complex div = new Complex();
+            div.Real = ((a.Real * b.Real) + (a.Im * b.Im))/(Math.Pow(b.Real,2)+Math.Pow(b.Im,2));
+            div.Im = ((a.Real * b.Im) - (a.Im * b.Real))/ (Math.Pow(b.Real, 2) + Math.Pow(b.Im, 2));
+            return div;
+        }
+        public static double Magnitude(Complex a)
+        {
+            double mag = Math.Sqrt(Math.Pow(a.Real, 2) + Math.Pow(a.Im, 2));
+
+            return mag;
+        }
+        public static double Phase(Complex a)
+        {
+
+            double phase=2*Math.Atan((Math.Sqrt(a.Real*a.Real+a.Im*a.Im)-a.Real)/a.Im);
+            return phase;
+        }
+    }
+}
--- ConsoleApp28/Program.cs
+++ ConsoleApp28/Program.cs
@@ -10,6 +10,26 @@ namespace ConsoleApp28
     {
         static void Main(string[] args)
         {
+            //Complex a = new Complex(3, 4);
+            //Console.WriteLine(a);
+            Complex a = new Complex(6, 7);
+            Complex b = new Complex(4, 5);
+
+            Console.WriteLine($"First Complex number` {a}");
+            Console.WriteLine($"Second Complex number` {b}");
+            Complex sum=Complex.Addition(a, b);
+            Console.WriteLine($"Addition of {a} and {b} is` {sum}");
+            Complex sub = Complex.Subtraction(a, b);
+            Console.WriteLine($"Subtraction of {a} and {b} is` {sub}");
+            Complex mult = Complex.Multiplication(a, b);
+            Console.WriteLine($"Multiplication of {a} and {b} is` {mult}");
+            Complex div = Complex.Division(a, b);
+            Console.WriteLine($"Division of {a} and {b} is` {div}");
+            double mag = Complex.Magnitude(a);
+            Console.WriteLine($"Magnitude of {a}  is` {mag}");
+            double phase = Complex.Phase(a);
+            Console.WriteLine($"Phase of {a}  is` {phase}");
+
         }
     }
 }
